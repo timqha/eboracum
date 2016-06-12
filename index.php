d2e604aef75a54518714370eb65e2878b9a23639
Olexander T <timqha@gmail.com>
2016-06-12 22:07:12 +0300

    dotted border shop checkout

--- index.php
+++ index.php
@@ -370,6 +370,145 @@
 
 
 
+<!-- SHOP -->
+<div class="basket">
+    <img src="<?php echo bloginfo('template_url'); ?>/img/basket.png" alt="bucket">
+    <div><a href="#">9</a></div>
+</div>
 
+<section class=" content-text CHECKOUT-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
 
-<?php //get_footer(); ?>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="barocks">
+                            <div class="barocks-img"><img src="<?php echo bloginfo('template_url'); ?>/img/atr-symbol.png"
+                                                          alt="art"></div>
+                            <div class="barocks-title">SHOP
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <table class="table-events">
+                            <tr>
+                                <td colspan="3" class="gg">shopping basket</td>
+                                <td>price</td>
+                                <td colspan="2">quantity</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="<?php echo bloginfo('template_url'); ?>/img/album-1.jpg"
+                                         alt="art">
+                                </td>
+                                <td class="tac">
+                                    <div class="big-number">
+                                        25
+                                    </div>
+                                    <div class="month">january</div>
+                                </td>
+                                <td class="line">
+                                    <div class="table-title">
+                                        music for good friday
+                                    </div>
+                                    <div>
+                                        Holy Trinity Church, Micklegate, York<br>
+                                        19.00 - 22.00
+                                    </div>
+                                </td>
+                                <td class="text-even">
+                                    adult ticket - £12
+                                </td>
+                                <td>
+                                        <select>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                        </select>
+                                </td>
+                                <td class="times">
+                                    <a href="#" class="check-close">&times;</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="<?php echo bloginfo('template_url'); ?>/img/album-2.jpg"
+                                         alt="art">
+                                </td>
+                                <td class="tac">
+                                    <div class="big-number">
+                                        16
+                                    </div>
+                                    <div class="month">march</div>
+                                </td>
+                                <td class="line">
+                                    <div class="table-title">
+                                        concert
+                                    </div>
+                                    <div>
+                                        tattershall castle, lincs.<br>
+                                        19.00 - 22.00
+                                    </div>
+                                </td>
+                                <td class="text-even">
+                                    concession ticket - £8
+                                </td>
+                                <td>
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </td>
+                                <td class="times">
+                                    <a href="#" class="check-close">&times;</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="<?php echo bloginfo('template_url'); ?>/img/album-1.jpg"
+                                         alt="art">
+                                </td>
+                                <td class="tac">
+                                    <div class="big-number">
+                                        23
+                                    </div>
+                                    <div class="month">april</div>
+                                </td>
+                                <td class="line">
+                                    <div class="table-title">
+                                        if music be the food of love
+                                    </div>
+                                    <div>
+                                        st benet’s church, cambridge<br>
+                                        19.00 - 22.00
+                                    </div>
+                                </td>
+                                <td class="text-even">
+                                    concession ticket - £8
+                                </td>
+                                <td>
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </td>
+                                <td class="times">
+                                    <a href="#" class="check-close">&times;</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
