11fc4e11a1b796be43300118ccf7ba1073bf733b
Olexander T <timqha@gmail.com>
2016-06-13 17:09:16 +0300

    shop success, shop checkout is done.

---
+++ shop-success.php
@@ -0,0 +1,146 @@
+
+<!-- SHOP -->
+<div class="basket">
+    <img src="<?php echo bloginfo('template_url'); ?>/img/basket.png" alt="bucket">
+    <div><a href="#">9</a></div>
+</div>
+
+<section class=" content-text CHECKOUT-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="barocks">
+                            <div class="barocks-img"><img src="<?php echo bloginfo('template_url'); ?>/img/atr-symbol.png"
+                                                          alt="art"></div>
+                            <div class="barocks-title">SHOP
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="title-summary">order SUMMARY</div>
+
+                        <div class="text-summary"><b>Thank you for your order! </b>You should get a confirmation through Paypal to your e-mail shortly.<br/>
+                            Merchandise is posted within 5 working days or receiving your order. Please collect tickets from the box office at the event by bringing your order number from the Paypal confirmation.</div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <table class="table-events">
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="<?php echo bloginfo('template_url'); ?>/img/album-1.jpg"
+                                         alt="art">
+                                </td>
+                                <td class="tac">
+                                    <div class="big-number">
+                                        25
+                                    </div>
+                                    <div class="month">january</div>
+                                </td>
+                                <td class="line">
+                                    <div class="table-title">
+                                        music for good friday
+                                    </div>
+                                    <div>
+                                        Holy Trinity Church, Micklegate, York<br>
+                                        19.00 - 22.00
+                                    </div>
+                                </td>
+                                <td class="text-even">
+                                    adult ticket - £12
+                                </td>
+                                <td>
+                                    <div class="summary-number">
+                                        1
+                                    </div>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="<?php echo bloginfo('template_url'); ?>/img/album-2.jpg"
+                                         alt="art">
+                                </td>
+                                <td class="tac">
+                                    <div class="big-number">
+                                        16
+                                    </div>
+                                    <div class="month">march</div>
+                                </td>
+                                <td class="line">
+                                    <div class="table-title">
+                                        concert
+                                    </div>
+                                    <div>
+                                        tattershall castle, lincs.<br>
+                                        19.00 - 22.00
+                                    </div>
+                                </td>
+                                <td class="text-even">
+                                    concession ticket - £8
+                                </td>
+                                <td>
+                                    <div class="summary-number">
+                                        1
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="<?php echo bloginfo('template_url'); ?>/img/album-1.jpg"
+                                         alt="art">
+                                </td>
+                                <td class="tac">
+                                    <div class="big-number">
+                                        23
+                                    </div>
+                                    <div class="month">april</div>
+                                </td>
+                                <td class="line">
+                                    <div class="table-title">
+                                        if music be the food of love
+                                    </div>
+                                    <div>
+                                        st benet’s church, cambridge<br>
+                                        19.00 - 22.00
+                                    </div>
+                                </td>
+                                <td class="text-even">
+                                    concession ticket - £8
+                                </td>
+                                <td>
+                                    <div class="summary-number">
+                                        1
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                        <div class="row padding-total-success">
+                            <div class="col-xs-12">
+                                <div class="pull-right">
+                                    <span class="total">total:</span><span class="total-number">
+                                        £43.95
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
