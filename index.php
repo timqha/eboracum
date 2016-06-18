392e2fb1738205c5e65bea38c69bbbc3bc046c02
Olexander T <timqha@gmail.com>
2016-06-18 13:41:35 +0300

    twitter hard style css

--- index.php
+++ index.php
@@ -98,7 +98,17 @@
             <div class="row">
                 <div class="col-md-4">
 
-                    <div id="example1"></div>
+
+                    <div class="barrocks-twitter">
+<!--                        118 characters, as will operate at 240 characters? -->
+<!--                        <div id="example0">"Final prep for our trip to #york tomorrow for King Arthur with Horrible Histories author, Terry Deary @yorkearlymusic @VisitYork #returnhome"</div>-->
+                                            <div id="example1"></div>
+                        <div class="footer-twitter">
+                            - @Eboracumbaroque <a href="https://twitter.com/eboracumbaroque"><i class="pull-right fa fa-twitter"></i></a>
+                        </div>
+                    </div>
+
+
 
 
 <!--<a class="twitter-timeline"  data-chrome="noheader nofooter noborders noscrollbar transparent" data-width="100%" data-height="236" data-dnt="true"-->
--- js/twitter.js
+++ js/twitter.js
@@ -210,100 +210,10 @@ var config1 = {
     "id": '743883738547884032',
     "domId": 'example1',
     "maxTweets": 1,
+    "showUser": false,
+    "showTime": false,
     "enableLinks": true,
+    "showInteraction": false,
     "showPermalinks": false
 };
 twitterFetcher.fetch(config1);
-
-
-// ##### Simple example 2 #####
-// A simple example to get my latest 5 of my favourite tweets and write to a
-// HTML element with id "talk". Also automatically hyperlinks URLS and user
-// mentions and hashtags but does not display time of post. We also make the
-// request to Twitter specifiying we would like results where possible in
-// English language.
-var config2 = {
-    "id": '743883738547884032',
-    "domId": 'example2',
-    "maxTweets": 5,
-    "enableLinks": true,
-    "showUser": true,
-    "showTime": true,
-    "lang": 'en'
-};
-twitterFetcher.fetch(config2);
-
-
-// ##### Simple example 3 #####
-// A simple example to get latest 5 tweets for #API tag and shows any images
-// attached to tweets.
-var config3 = {
-    "id": '743883738547884032',
-    "domId": 'example3',
-    "maxTweets": 5,
-    "enableLinks": true,
-    "showImages": true
-};
-twitterFetcher.fetch(config3);
-
-
-// ##### Advanced example #####
-// An advance example to get latest 5 posts using hashtag #API and write to a
-// HTML element with id "tweets2" without showing user details and using a
-// custom format to display the date/time of the post, and does not show
-// retweets.
-var config4 = {
-    "id": '743883738547884032',
-    "domId": 'example4',
-    "maxTweets": 3,
-    "enableLinks": true,
-    "showUser": false,
-    "showTime": true,
-    "dateFunction": dateFormatter,
-    "showRetweet": false
-};
-
-// For advanced example which allows you to customize how tweet time is
-// formatted you simply define a function which takes a JavaScript date as a
-// parameter and returns a string!
-// See http://www.w3schools.com/jsref/jsref_obj_date.asp for properties
-// of a Date object.
-function dateFormatter(date) {
-    return date.toTimeString();
-}
-
-twitterFetcher.fetch(config4);
-
-
-// ##### Advanced example 2 #####
-// Similar as previous, except this time we pass a custom function to render the
-// tweets ourself! Useful if you need to know exactly when data has returned or
-// if you need full control over the output.
-
-var config5 = {
-    "id": '743883738547884032',
-    "domId": '',
-    "maxTweets": 3,
-    "enableLinks": true,
-    "showUser": true,
-    "showTime": true,
-    "dateFunction": '',
-    "showRetweet": false,
-    "customCallback": handleTweets,
-    "showInteraction": false
-};
-
-function handleTweets(tweets) {
-    var x = tweets.length;
-    var n = 0;
-    var element = document.getElementById('example5');
-    var html = '<div width="100%">';
-    while (n < x) {
-        html += '<span>' + tweets[n] + '</span>';
-        n++;
-    }
-    html += '</div>';
-    element.innerHTML = html;
-}
-
-twitterFetcher.fetch(config5);
