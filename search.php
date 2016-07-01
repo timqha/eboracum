28242628c583fa1363f11a6a13c6918f1f099a7c
Cruel <molo4nig@gmail.com>
2016-07-01 14:40:40 +0300

    Revert "search + category img"
    This reverts commit e5dadf06751da96c13391cd570e69fb9975eb60d.

--- search.php
+++ search.php
@@ -2,46 +2,25 @@
 <?php get_header("custom2"); ?>
 <div class="container">
     <div class="content">
-		<h1><?php echo 'Search result: ' . '<span>' . get_search_query() . '</span>'; ?></h1>
+		<h1><?php echo 'Результат поиска: ' . '<span>' . get_search_query() . '</span>'; ?></h1>
         <?php
         if (have_posts()) :
             while (have_posts()) : the_post();
                 ?>
-                 <div class="row">
-                            <div class="col-lg-12 post-blog">
-                                <div class="col-md-4">
-                                    <div class="title-post font-roman">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php the_title(); ?>
-                                        </a>
-                                    </div>
-
-                                    <div class="date-post">
-                                        <?php the_date(); ?>
-                                    </div>
-
-                                    <?php
-                                    if (comments_open() || get_comments_number()) : {
-                                        echo('<div class="badge-post">');
-                                        echo(get_comments_number());
-                                        echo('</div>');
-                                    }
-                                    endif; ?>
-
-                                </div>
-                                <div class="col-md-8">
-
-                                    <div class="post-content">
-                                        <?php echo first_post_image() ?>
-                                        <?php the_excerpt(); ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div id="posts">
+                    <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+                    <div id="post_info">
+                        <div>Автор: <?php the_author() ?></div>
+                        <div>Дата добавления: <?php the_date() ?></div>
+                        <div id="clear"></div>
+                    </div>
+                    <p><?php the_excerpt() ?></p>
+                    <span>Категория: <?php the_category(', ') ?></span>
+                </div>
             <?php endwhile; ?>
         <?php
         else :
-            echo "<div class='row'> <div class='container'>No results for this </div></div>";
+            echo "Извините по Вашему результату ничего не найдено";
         endif;
         ?>	
 </div>
